ebc7f8156b02a4120f7a1aabf6ee723af7a7d2cd
xch <616639414@qq.com>
2016-12-28 18:50:58 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <div class="tab">
+      I am tab
+    </div>
+    <div class="content">
+      I am content
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from 'components/header/Header.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Header
+  },
+  data (){
+    return {
+      msg:"hello vue"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/header/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header">
+    <span>我是header</span>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
